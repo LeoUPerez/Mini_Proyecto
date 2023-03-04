9d5346429b11a9634ab0fa8b6214b604ac396a81
Leo <leo0223perez@outlook.com>
2023-03-04 13:13:48 -0400

    Updated

--- frontend/Mini_Proyecto/src/Components/NavBar.jsx
+++ frontend/Mini_Proyecto/src/Components/NavBar.jsx
@@ -3,33 +3,33 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   function selector_in(e) {
-    let lk = e.target;
+    let target = e.target;
 
-    if (lk.id === "prueba") {
-      let id = lk.children[0].id;
-      let prueba = document.getElementById(id);
-      prueba.className = "w-1.5 h-1.5 rounded-full bg-blue-600";
+    if (target.id === "Link") {
+      let id = target.children[0].id;
+      let elemento = document.getElementById(id);
+      elemento.className = "w-1.5 h-1.5 rounded-full bg-blue-600";
     }
   }
   function selector_out(e) {
-    let lk = e.target;
+    let target = e.target;
 
-    if (lk.id === "prueba") {
-      let id = lk.children[0].id;
-      let prueba = document.getElementById(id);
-      prueba.className = "w-1.5 h-1.5 rounded-full";
+    if (target.id === "Link") {
+      let id = target.children[0].id;
+      let elemento = document.getElementById(id);
+      elemento.className = "w-1.5 h-1.5 rounded-full";
     }
   }
 
   return (
     <section className=" w-2/4 h-full flex justify-center items-center gap-7 bg-gray-100 font-light rounded-xl pb-2">
       <Link
-        id="prueba"
+        id="Link"
         onMouseEnter={selector_in}
         onMouseOut={selector_out}
-        className="hover:0 transition-all duration-300 flex items-center flex-col justify-center"
+        className="hover:text-blue-600 transition-all duration-300 flex items-center flex-col justify-center"
       >
-        <div id="link1" className="w-1.5 h-1.5 rounded-full"></div>
+        <div id="Link1" className="w-1.5 h-1.5 rounded-full"></div>
         Home
       </Link>
       {/* <Link
@@ -53,30 +53,30 @@ const NavBar = () => {
         Link_3
       </Link> */}
       <Link
-        id="prueba"
+        id="Link"
         onMouseEnter={selector_in}
         onMouseOut={selector_out}
         className="hover:text-blue-600 transition-all duration-300 flex items-center flex-col justify-center"
       >
-        <div id="link4" className="w-1.5 h-1.5 rounded-full"></div>
+        <div id="Link4" className="w-1.5 h-1.5 rounded-full"></div>
         Servicios
       </Link>
       <Link
-        id="prueba"
+        id="Link"
         onMouseEnter={selector_in}
         onMouseOut={selector_out}
         className="hover:text-blue-600 transition-all duration-300 flex items-center flex-col justify-center"
       >
-        <div id="link5" className="w-1.5 h-1.5 rounded-full"></div>
+        <div id="Link5" className="w-1.5 h-1.5 rounded-full"></div>
         Sobre Nosotros
       </Link>
       <Link
-        id="prueba"
+        id="Link"
         onMouseEnter={selector_in}
         onMouseOut={selector_out}
         className="hover:text-blue-600 transition-all duration-300 flex items-center flex-col justify-center"
       >
-        <div id="link6" className="w-1.5 h-1.5 rounded-full"></div>
+        <div id="Link6" className="w-1.5 h-1.5 rounded-full"></div>
         Contactos
       </Link>
     </section>
